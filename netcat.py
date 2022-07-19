ea08adf34dd6bd74a791b3b51d92e4dbd31511aa
honx1337 <109291174+honx1337@users.noreply.github.com>
2022-07-19 12:42:54 +0200

    braces

--- netcat.py
+++ netcat.py
@@ -124,7 +124,7 @@ if __name__ == '__main__':
         buffer = sys.stdin.read()
 
 nc = NetCat(args, buffer.encode('utf-8'))
-nc.run
+nc.run()
 #something not working
 #after inputting python3 netcat.py -t 192.168.1.203 -p 5555 -l -c on Kali and python3 netcat.py -t 192.168.1.203 -p 5555 on target I should get shell
 #but the script just ends
