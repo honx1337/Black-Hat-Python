7b2739ac5259376cbac892aee24972f5abf6653b
honx1337 <109291174+honx1337@users.noreply.github.com>
2022-08-22 00:45:10 +0200

    trying bing
    nie chcą mi dać klucza API :/

---
+++ Chapter 6/bhp_bing.py
@@ -0,0 +1,12 @@
+from burp import IBurpExtender
+from burp import IContextMenuFactory
+
+from java.net import URL
+from java.util import ArrayList
+from javax.swing import JMenuItem
+from thread import start_new_thread
+
+import json
+import socket
+import urllib
+bing_api_key = ""
